1e30de6a2bae7fff080b46d5270e329325bbf397
Afsoon Taghizadehgan <tagh0015@algonquinlive.com>
2023-04-17 11:45:22 -0400

    getting weather info from API, no CSS used for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Weather from './components/Weather'
+import React from 'react'
 
 function App() {
+  
+  const [city, setCity]= React.useState("")
+
+  function setCityInput(event){
+    if (event.key === 'Enter') {
+        // 👇 Get input value
+        setCity(event.target.value)
+      }
+    
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <input 
+                type="text" 
+                className="city--input" 
+                placeholder='Enter a city'
+                onKeyDown={setCityInput} 
+                name="city"
+                />
+      <Weather 
+      city={city}/>
     </div>
   );
 }
