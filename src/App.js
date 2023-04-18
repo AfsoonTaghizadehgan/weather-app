392a2b5ce61dec9ccadd9c90a6d0feea8e9ea822
Afsoon Taghizadehgan <tagh0015@algonquinlive.com>
2023-04-18 11:33:49 -0400

    input is in seperate file, 404 error is handled

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 
 import './App.css';
 import Weather from './components/Weather'
+import Weatherinput from './components/WeatherInput';
 import React from 'react'
 
 function App() {
-  
   const [city, setCity]= React.useState("")
-
   function setCityInput(event){
     if (event.key === 'Enter') {
-        // 👇 Get input value
-        setCity(event.target.value)
-      }
-    
+    // 👇 Get input value
+      setCity(event.target.value)
+  }
 }
   return (
     <div className="App" >
-      <input 
-                type="text" 
-                className="city--input" 
-                placeholder='Enter a city'
-                onKeyDown={setCityInput} 
-                name="city"
-                />
+      <Weatherinput
+      setCityInput={setCityInput}
+      city={city}
+      />
       <Weather 
       city={city}/>
     </div>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,6 @@
 import '../App.css';
 import React from 'react';
-import partialCloud from './images/partialCloud.png'
+//import partialCloud from './images/partialCloud.png'
 
 export default function Weather(props) {
 
@@ -12,48 +12,68 @@ export default function Weather(props) {
         description:"",
         name:""
         })
-    React.useEffect(()=>{
-    async function getWeatherData() {
-        if(props.city!==""){
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=d914a0d6566efc1d7f49f4af49f8fe96&units=metric`)
-        const data = await res.json()
+        const [hasError, setHasError] = React.useState(false);
 
-         setWeatherData(preveState => ({
-            ...preveState,
-            temp: data.main.temp,
-            feelsLike: data.main.feels_like,
-            windSpeed: data.wind.speed,
-            description: data.weather[0].description,
-            name: data.name,
-            humidity: data.main.humidity
-            }))
-            console.log(data)  
+    React.useEffect(()=>{
+        const fetchData = async () => {
+            setHasError(false)
+            if(props.city!==""){
+                const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=d914a0d6566efc1d7f49f4af49f8fe96&units=metric`)
+                const data = await res.json()
+                if(data.cod !== "404"){
+                    setWeatherData(preveState => ({
+                        ...preveState,
+                        temp: data.main.temp,
+                        feelsLike: data.main.feels_like,
+                        windSpeed: data.wind.speed,
+                        description: data.weather[0].description,
+                        name: data.name,
+                        humidity: data.main.humidity
+                        }))
+                    console.log(data)
+                }  
+                else{
+                    setWeatherData("")
+                    setHasError(true)
+                        
+                }
+        }
     }
-}
-    getWeatherData()
+    fetchData();
 }, [props.city, weatherData.temp])
 
-    return(
-        <main className=''
-        style={{ 
-            backgroundImage: `url(${partialCloud})` 
-          }}>
-            <div className='city--form'>
-                {/* <img src={partialCloud}></img> */}
-                { props.city!=="" && 
-                <div >
-                    <h2>{props.city}</h2>
-                    <p>{weatherData.description}</p>
-                    <p>{weatherData.name}</p>
-                    <p>{weatherData.temp}</p>
-                    <p>{weatherData.feelsLike}</p>
-                    <p>{weatherData.humidity}</p>
-                    <p>{weatherData.windSpeed}</p>
-                </div>}
-    
-        </div>
-    </main>
-    )
+    if(hasError === false){    
+        return(
+            <main className=''
+            // style={{ 
+            //     backgroundImage: `url(${partialCloud})` 
+            //   }}
+            >
+                <div className='city--form'>
+                    {/* <img src={partialCloud}></img> */}
+                    { props.city!=="" &&
+                    <div >
+                        <h2>{props.city}</h2>
+                        <p>{weatherData.description}</p>
+                        <p>{weatherData.name}</p>
+                        <p>{weatherData.temp}</p>
+                        <p>{weatherData.feelsLike}</p>
+                        <p>{weatherData.humidity}</p>
+                        <p>{weatherData.windSpeed}</p>
+                    </div>}
+        
+            </div>
+        </main>
+        )
+    }
+    else{
+        return(
+            <div >
+                <br></br>
+                <p>Please enter a valid city</p>
+            </div>
+        )
+    }    
 
    
 
